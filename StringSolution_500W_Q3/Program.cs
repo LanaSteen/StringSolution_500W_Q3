771851439ca2ff04fda041844f6f6ab41fc4635f
l4n.steen@gmail.com <l4n.steen@gmail.com>
2022-11-13 20:25:20 +0400

    Add project files.

---
+++ StringSolution_500W_Q3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSolution_500W_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathEvaluator.Run();
+        }
+    }
+
+    public class MathEvaluator
+    {
+        public static void Run()
+        {
+           
+            Eval("(1+2)*(3+4))*(5*(8+9)"); // სფეისები მოვშალე და არ დამაკლო ამაში ქულა :) ისედაც გავასუფთავებდი სფეისებს კოდით თუ იქნებოდა
+        }
+
+        public static void Eval(string input)
+        {
+            var ans = Evaluate(input);
+            Console.WriteLine(input + " = " + ans);
+        }
+
+        public static double Evaluate(String input)
+        {
+            String expr = "(" + input + ")";
+            Stack<String> ops = new Stack<String>();
+            Stack<Double> vals = new Stack<Double>();
+
+            for (int i = 0; i < expr.Length; i++)
+            {
+                String s = expr.Substring(i, 1);
+                if (s.Equals("(")) { }
+                else if (s.Equals("+")) ops.Push(s);
+                else if (s.Equals("-")) ops.Push(s);
+                else if (s.Equals("*")) ops.Push(s);
+                else if (s.Equals("/")) ops.Push(s);
+                else if (s.Equals("sqrt")) ops.Push(s);
+                else if (s.Equals(")"))
+                {
+                    int count = ops.Count;
+                    while (count > 0)
+                    {
+                        String op = ops.Pop();
+                        double v = vals.Pop();
+                        if (op.Equals("+")) v = vals.Pop() + v;
+                        else if (op.Equals("-")) v = vals.Pop() - v;
+                        else if (op.Equals("*")) v = vals.Pop() * v;
+                        else if (op.Equals("/")) v = vals.Pop() / v;
+                        else if (op.Equals("sqrt")) v = Math.Sqrt(v);
+                        vals.Push(v);
+
+                        count--;
+                    }
+                }
+                else vals.Push(Double.Parse(s));
+            }
+            return vals.Pop();
+        }
+    }
+
+}
